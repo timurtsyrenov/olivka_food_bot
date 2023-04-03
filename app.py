d11d9e2788cb4746339726b5ec2357cb82b8f3c5
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2023-04-03 17:54:14 +0700

    Убрал дни.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from aiogram import executor
 from handlers import dp
+from handlers.admin.notify_admins import \
+    on_startup_notify  # Импортируем функцию, которая отправляет сообщение о запуске бота всем админам
+from utils.set_bot_commands import \
+    set_default_commands  # Импортируем функцию, которая устанавливает команды для бота
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
@@ -8,12 +12,8 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(mes
 # Основной файл
 async def on_startup(dp):  # Создаем асинхронную функцию которая будет запускаться по запуску бота
 
-    from utils.notify_admins import \
-        on_startup_notify  # Импортируем функцию которая отправляет сообщение о запуске бота всем админам
     await on_startup_notify(dp)
 
-    from utils.set_bot_commands import \
-        set_default_commands  # Импортируем функцию которая устанавливает команды для бота
     await set_default_commands(dp)
 
     logging.info('Бот запущен')
