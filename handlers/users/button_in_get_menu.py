d6dcd6a0fc4a04a7410b6943eebb427624e7c528
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2023-04-04 10:56:49 +0700

    Изменил запрос на текущую дату

--- handlers/users/button_in_get_menu.py
+++ handlers/users/button_in_get_menu.py
@@ -3,13 +3,16 @@ from loader import dp, bot
 import utils
 import datetime
 
-number_today = datetime.datetime.today().weekday() + 1  # Переменная, хранящая номер сегодняшнего дня недели
+
+def today():
+    return datetime.datetime.today().weekday() + 1  # Переменная, хранящая номер сегодняшнего дня недели
 
 
 # number_today = 6
 
 @dp.message_handler(text='/today')  # Cоздаем message handler который ловит команду /today
 async def button_today(message: types.Message):  # Создаем асинхронную функцию, которая отправляет меню на сегодня
+    number_today = today()
     if number_today in [6, 7]:  # Проверка на субботу и воскресенье
         await message.answer('На выходных не кормят')
         await bot.send_sticker(chat_id=message.from_user.id,
