219586e69740fcd00aa00dfce769bf8815736995
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2023-04-05 14:03:18 +0700

    Добавлены комментарии, исправлены некоторые type

--- handlers/users/button_in_get_menu.py
+++ handlers/users/button_in_get_menu.py
@@ -4,23 +4,33 @@ import utils
 import datetime
 
 
-def today():
-    return datetime.datetime.today().weekday() + 1  # Переменная, хранящая номер сегодняшнего дня недели
+def get_today() -> int:
+    """
+    Функция возвращает текущую дату
+    :return today: текущая дата в виде числа
+    """
+    today = datetime.datetime.today().weekday() + 1
+    return today
 
-
-# number_today = 6
 
 @dp.message_handler(text='/today')  # Cоздаем message handler который ловит команду /today
-async def button_today(message: types.Message):  # Создаем асинхронную функцию, которая отправляет меню на сегодня
-    number_today = today()
+async def button_today(message: types.Message):
+    """
+    Асинхронная функция, которая отправляет меню на сегодня
+    :parameter message: Сообщение от пользователя
+    :return: Отправляет сообщение пользователю
+    """
+    number_today = get_today()
     if number_today in [6, 7]:  # Проверка на субботу и воскресенье
         await message.answer('На выходных не кормят')
         await bot.send_sticker(chat_id=message.from_user.id,
                                sticker=r"CAACAgIAAxkBAAEIVUlkIH22b1zwyhnkOPttEAMkc28UeQAC8xAAAnt4yUv8CBg5xaTu4C8E")
     else:
         menu = utils.get_menu_to_dict(number_today)
-        menu_lunch = "\n".join(str(item) for item in menu.get("lunch").get("food"))
-        menu_dinner = "\n".join(str(item) for item in menu.get("dinner").get("food"))
+        menu_lunch = "\n".join(str(item) for item in menu.get("lunch").get("food"))  # Формируем строку за 450 рублей
+        # из всех блюд для отправки пользователю
+        menu_dinner = "\n".join(str(item) for item in menu.get("dinner").get("food"))  # Формируем строку за 300 рублей
+        # из всех блюд для отправки пользователю
         await message.answer(
             f'Меню на сегодня: \n'
             f'{menu.get("lunch").get("name")} - {menu.get("lunch").get("price")}\n'
--- utils/parser/parser.py
+++ utils/parser/parser.py
@@ -1,5 +1,6 @@
 import logging
 
+import bs4
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,15 +8,21 @@ from bs4 import BeautifulSoup
 Скрипт, получающий html страницу и парсящий ее в списки обедов по дням недели.
 '''
 
-url = 'https://olivkafood.ru/page/menu/6'
+url = 'https://olivkafood.ru/page/menu/6'  # Путь к странице сайта Оливки
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                   '(KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.2.998 Yowser/2.5 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
-}
+}  # Заголовки запроса
 
 
-def request_page(url_site, headers_bot):
+def request_page(url_site: str, headers_bot:  dict[str, str]) -> BeautifulSoup:
+    """
+    Получение страницы сайта, полученная через библиотеку парсинга lxml
+    :param str url_site: Адрес сайта
+    :param dict[str, str] headers_bot: Заголовки запроса
+    :return BeautifulSoup soup: Страница сайта
+    """
     try:
         r = requests.get(url=url_site, headers=headers_bot, timeout=10)  # посылаем запрос
         if r.content is not None:
@@ -24,7 +31,7 @@ def request_page(url_site, headers_bot):
             return soup
         else:
             logging.error('Нет данных от сайта Оливки')
-            return 'Нет данных от сайта Оливки'  # нужно передать админу? или всем пользователям
+            # return 'Нет данных от сайта Оливки' # нужно передать админу? или всем пользователям
     except requests.exceptions.HTTPError as e:
         logging.error(f'HTTP error: {e}')
     except requests.exceptions.ConnectionError as e:
@@ -34,6 +41,11 @@ def request_page(url_site, headers_bot):
 
 
 def get_menu_to_dict(day_number: int) -> dict:
+    """
+    Получение состава обеда за запрашиваемый день недели
+    :param int day_number: Номер запрашиваемого дня недели
+    :return dict return_dict: Словарь с данными обеда за запрошенный день недели
+    """
     lunch_dict = dict()
     dinner_dict = dict()
     return_dict = dict()
@@ -41,9 +53,16 @@ def get_menu_to_dict(day_number: int) -> dict:
     soup = request_page(url, headers)  # получаем html страницу
     # logging.debug(soup)
 
-    menu = soup.find_all('div', class_=f'menu-item mix menu-category-filter c{day_number}', limit=2)
+    menu = soup.find_all('div', class_=f'menu-item mix menu-category-filter c{day_number}', limit=2)  # Ищем тег 'div'
+    # классом 'menu-item mix menu-category-filter c{номер дня}' в запрошенной странице с указанием дня недели
 
-    def add_to_dict(name_dict: dict, items: object, price: str) -> None:
+    def add_to_dict(name_dict: dict, items: bs4.ResultSet, price: str) -> None:
+        """
+        Добавление данных по обедам в словарь lunch_dict или dinner_dict
+        :param dict name_dict: Словарь с данными обеда за запрошенный день недели
+        :param bs4.ResultSet items: Список блюд
+        :param str price: Стоимость обеда
+        """
         item_list = list()
         for item in items[1:]:
             if item.text != '':
