cdc7d4c8dc9ed9aac481d4eee19ec3258b006f1a
timur <timurmurmurik@mail.ru>
2023-04-05 23:35:38 +0700

    Добавил преобразователь текста в изображение

--- handlers/users/button_in_get_menu.py
+++ handlers/users/button_in_get_menu.py
@@ -1,4 +1,6 @@
 from aiogram import types
+from aiogram.types import InputFile
+
 from loader import dp, bot
 import utils
 import datetime
@@ -30,10 +32,11 @@ async def button_today(message: types.Message):
         # из всех блюд для отправки пользователю
         menu_dinner = "\n".join(str(item) for item in menu.get("dinner").get("food"))  # Формируем строку за 300 рублей
         # из всех блюд для отправки пользователю
-        await message.answer(
-            f'Меню на сегодня: \n'
-            f'{menu.get("lunch").get("name")} - {menu.get("lunch").get("price")}\n'
-            f'{menu_lunch}\n\n'
-            f'{menu.get("dinner").get("name")} - {menu.get("dinner").get("price")}\n'
-            f'{menu_dinner}'
-        )
+        menu_to_convert = f'Меню на сегодня: \n' \
+                          f'{menu.get("lunch").get("name")} - {menu.get("lunch").get("price")}\n' \
+                          f'{menu_lunch}\n\n' \
+                          f'{menu.get("dinner").get("name")} - {menu.get("dinner").get("price")}\n' \
+                          f'{menu_dinner}'
+        utils.convert_text_to_image(menu_to_convert)
+        photo_bytes = InputFile(path_or_bytesio='utils/converter/menu_image.png')
+        await dp.bot.send_photo(chat_id=message.chat.id, photo=photo_bytes)
