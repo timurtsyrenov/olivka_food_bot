572f9677568c66ed0135af04c1d2f087093a1a42
Василиненко Вадим Сергеевич <vadim.vasilinenko@eltex.loc>
2023-04-17 13:40:07 +0700

    Добавил фон и заменил шрифт

--- data/config.py
+++ data/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 BOT_TOKEN = str(os.getenv(key="BOT_TOKEN"))
 ADMIN_ID = int(os.getenv(key="ADMIN_ID"))
-FONT_LINK = os.getenv(key="FONT_LINK", default="utils/converter/Ubuntu-R.ttf")
+FONT_LINK = os.getenv(key="FONT_LINK", default="utils/converter/Garet.ttf")
 LOG_LEVEL = str(os.getenv(key="LOG_LEVEL", default="DEBUG"))
