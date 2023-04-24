485e07d7203a63c56c01d1c9048b110a4466fc51
timur <timurmurmurik@mail.ru>
2023-04-24 23:37:10 +0700

    создание таблицы и составление примерной структуры функций скрипта

---
+++ database/sqlite.py
@@ -0,0 +1,66 @@
+import sqlite3 as sl
+
+# Инициализируем переменную базы данных и создаем ее файл
+with sl.connect("sqlite.db") as con:
+    cur = con.cursor()
+    # Создаем таблицу, первичным ключем является chat id
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS notification (
+        chat_id INTEGER NOT NULL PRIMARY KEY,
+        on_off INTEGER NOT NULL DEFAULT 1,
+        time TEXT NOT NULL DEFAULT "11:00")
+        """
+    )
+
+# Команда удаления таблицы
+# cur.execute("""DROP TABLE notification""")
+
+
+def create_new_user(chat_id: int):
+    """
+    Функция добавляющая нового пользователя в базу данных при вводе команды start
+    :param int chat_id: chat id пользователя
+    :return:
+    """
+    # Формируем данные записи
+    data = [(chat_id, 1, "10:00")]
+    # Подготавливаем множественный запрос
+    sql = "INSERT INTO notification (CHAT_ID, ON_OFF, TIME) values(?, ?, ?)"
+    # Загружаем данные в базу
+    with cur:
+        cur.executemany(sql, data)
+    con.commit()
+    # вывод данных
+    # with db:
+    #     data = db.execute("SELECT * FROM notification")
+    #     for row in data:
+    #         print(row)
+
+
+# create_new_user(123123)
+
+
+def on_notification(chat_id: int):
+    """
+    Функция для внесения в поле on_off значения 1(on)
+    :param int chat_id: chat id пользователя
+    :return:
+    """
+    pass
+
+def off_notification(chat_id: int):
+    """
+    Функция для внесения в поле on_off значения 0(off)
+    :param int chat_id: chat id пользователя
+    :return:
+    """
+    pass
+
+def set_time_notification(chat_id: int, time: str):
+    """
+    Функция для внесения в поле time пользовательского времени
+    :param int chat_id: chat id пользователя
+    :param str time: устанавливаемое время в формате HH:MM
+    :return:
+    """
+    pass
