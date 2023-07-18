913a8a396a97d68c75cf2cd84a3020c5178fb723
timur <timurmurmurik@mail.ru>
2023-07-18 21:20:33 +0700

    Доработка функции set_custom_time

--- handlers/users/set_custom_time.py
+++ handlers/users/set_custom_time.py
@@ -15,9 +15,16 @@ async def set_custom_time(message: types.Message):
     :parameter message: Сообщение от пользователя
     """
     logger.info(f"Пользователя с chat_id = {message.chat.id} хочет сменить время рассылки")
-    time_str = message.text.split(" ")[1]
     try:
-        datetime.strptime(time_str, "%H:%M")
-        await set_custom_time_in_db(message.chat.id, time_str)
-    except ValueError:
-        await message.answer("Просьба ввести время в формате: HH:MM")
+        time_str = message.text.split(" ")[1]
+        try:
+            datetime.strptime(time_str, "%H:%M")
+            await set_custom_time_in_db(message.chat.id, time_str)
+            await message.answer(f"Установлено время рассылки {time_str}")
+            logger.info(f"Пользователь с chat_id = {message.chat.id} сменил время рассылки на {time_str}")
+        except ValueError:
+            await message.answer("Необходимо ввести команду в формате /set_custom_time HH:MM")
+
+    except IndexError:
+        await message.answer("Необходимо ввести команду в формате /set_custom_time HH:MM")
+
